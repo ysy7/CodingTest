de83d23c676176281e2aecb0058d894af030c193
MiJi5 <Miji5CodingTest>
2022-03-21 12:19:26 +0900

    문자의 갯수 구하는 strlen()

--- CodingTest/00 출력함수.c
+++ CodingTest/00 출력함수.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 
 int main(void)
 {
@@ -9,14 +9,16 @@ int main(void)
 	float sosu1 = 0.001592f;
 	// 문자는 띄어쓰기 포함해서 11개
 	// 배열의 길이는 11+1(널문자) = 12
-	char mjyeol[] = "Hello World";
+	char mjyeol[] = "Hello World";	// 문자갯수 11개
 
 	printf("정수 %d, 문자 %c, 더블 %lf, 플롯 %f\n"
 		, jungsu, munja, sosu, sosu1);
 	
 	//printf("%s\n", mjyeol);
 	// mjyeol를 %c로 찍어보자
-	for (int i = 0; i < 12; i++)
+	// [0]:'H', [1]:'e', [2]:'l', [3]:'l', [4]:'o', [5]:' '
+	// [6]:'W', [7]:'o', [8]:'r', [9]:'l', [10]:'d', [11]:'\0'
+	for (int i = 0; i < strlen(mjyeol); i++)
 	{
 		printf("%c", mjyeol[i]);
 	}
