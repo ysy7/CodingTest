d46ce29f506a81eb0f60ecf50238418ba47ceee6
MiJi5 <Miji5CodingTest>
2022-03-21 11:33:50 +0900

    %d %c %lf %f

---
+++ CodingTest/00 출력함수.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int jungsu = 1;
+	char munja = 'a';
+	double sosu = 3.14;
+	float sosu1 = 0.001592f;
+
+	printf("정수 %d, 문자 %c, 더블 %lf, 플롯 %f\n"
+		, jungsu, munja, sosu, sosu1);
+}
