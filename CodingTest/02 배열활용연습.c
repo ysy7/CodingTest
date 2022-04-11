36dfdf0bd23c01db45d38adbfb98ebb5c32d37bf
ysy <ysy2471@gmil.com>
2022-04-11 12:11:51 +0900

    최대값 구하는 것을 함수화

--- CodingTest/02 배열활용연습.c
+++ CodingTest/02 배열활용연습.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
-int main(void)
+void getMax(int* arr, int length) 
 {
-	//최대값은 14(arr[9])
-	int arr[10] = { 8,5,1,6,4,9,10,11,14 };
-
 	int temp = -1;   //비교대상
 	int idx = 99;    //인덱스 값
 
 	//배열 요소중 최대값과 그에 해당하는 idx값을 구하시오
 
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < length; i++)
 	{
 		if (arr[i] > temp)
 		{
@@ -19,8 +16,17 @@ int main(void)
 		}
 	}
 
-	printf("같은 : % D 인덱스 값은 % D \n", temp, idx);
-		
+	printf("같은 : %d 인덱스 값은 %d \n", temp, idx);
+
+}
+
+int main(void)
+{
+	//최대값은 14(arr[9])
+	int arr1[10] = { 8,5,1,6,4,9,10,11,14 };
+	getMax(arr1, 10);
+
+	
 	
 	
 	return 0;
